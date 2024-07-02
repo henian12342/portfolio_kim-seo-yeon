7e1c5aa312151164e205930fa9a629fbae86e9aa
henian12342 <rlatjdus7899@gmail.com>
2024-07-02 15:21:17 +0900

    var 1.1.0

--- js/main.js
+++ js/main.js
@@ -17,30 +17,6 @@ $(document).ready(function(){
 	})
 
 
-	// 영역 도달 시 해당하는 메뉴 활성화
-	var homeSection = $("#home").offset().top;
-	var aboutSection = $("#about").offset().top;
-	var projectSection = $("#project").offset().top;
-	var contactSection = $("#contact").offset().top;
-
-	$(window).scroll(function(){
-		var scrollNow = $(window).scrollTop();
-
-		if ( scrollNow >= homeSection && scrollNow < aboutSection) {
-			$("header .nav li").removeClass('on');
-			$("header .nav li").eq(0).addClass('on');
-		}else if ( scrollNow >= aboutSection && scrollNow < projectSection) {
-			$("header .nav li").removeClass('on');
-			$("header .nav li").eq(1).addClass('on');
-		}else if ( scrollNow >= projectSection && scrollNow < contactSection ) {
-			$("header .nav li").removeClass('on');
-			$("header .nav li").eq(2).addClass('on');
-		}else if ( scrollNow >= contactSection ) {
-			$("header .nav li").removeClass('on');
-			$("header .nav li").eq(3).addClass('on');
-		}
-	});
-
 		// 메뉴 클릭 시 해당 섹션으로 이동
 		$(".nav li a").click(function(event){
 			event.preventDefault();
